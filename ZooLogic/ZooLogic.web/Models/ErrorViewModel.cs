12358b1253e0634970d479325b59083cf20b47f1
IvanVeselinov-Code <Ivanveselinov264@gmail.com>
2025-03-20 11:34:42 +0200

    Initial Project Skeleton

---
+++ ZooLogic/ZooLogic.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZooLogic.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
